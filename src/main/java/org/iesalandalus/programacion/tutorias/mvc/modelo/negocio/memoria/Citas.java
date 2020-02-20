d0b2a89bdcfb5a391a8a7899d9d73972fe398424
Mar <Mar@DESKTOP-7OECT65.home>
2020-02-20 15:58:10 +0100

    Fichero README, copia de ficheros de la clase anterior a esta.

---
+++ src/main/java/org/iesalandalus/programacion/tutorias/mvc/modelo/negocio/memoria/Citas.java
@@ -0,0 +1,123 @@
+package org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.memoria;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.tutorias.mvc.modelo.dominio.Alumno;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.dominio.Cita;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.dominio.Profesor;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.dominio.Sesion;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.dominio.Tutoria;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.ICitas;
+
+public class Citas implements ICitas {
+
+	private List<Cita> coleccionCitas;
+
+	public Citas() {
+		coleccionCitas = new ArrayList<>();
+	}
+
+	@Override
+	public List<Cita> get() {
+		List<Cita>citasOrdenadas = copiaProfundaCitas();
+		Comparator<Profesor> comparadorProfesor = Comparator.comparing(Profesor::getDni);
+		Comparator<Tutoria> comparadorTutoria = Comparator.comparing(Tutoria::getProfesor, comparadorProfesor).thenComparing(Tutoria::getNombre);
+		Comparator<Sesion>comparadorSesion = Comparator.comparing(Sesion::getTutoria,comparadorTutoria).thenComparing(Sesion::getFecha);
+		citasOrdenadas.sort(Comparator.comparing(Cita::getSesion,comparadorSesion).thenComparing(Cita::getHora));
+		return citasOrdenadas;
+	}
+
+	private List<Cita> copiaProfundaCitas() {
+		List<Cita> copiaCita = new ArrayList<>();
+		for (Cita cita : coleccionCitas) {
+			copiaCita.add(new Cita(cita));
+		}
+		return copiaCita;
+	}
+
+	@Override
+	public List<Cita> get(Sesion sesion) {
+		if (sesion == null) {
+			throw new NullPointerException("ERROR: La sesión no puede ser nula.");
+		}
+		List<Cita> citasSesion = new ArrayList<>();
+		for (Cita cita : coleccionCitas) {
+			if (cita.getSesion().equals(sesion)) {
+				citasSesion.add(new Cita(cita));
+			}
+		}
+		citasSesion.sort(Comparator.comparing(Cita::getHora));
+		return citasSesion;
+	}
+
+	@Override
+	public List<Cita> get(Alumno alumno) {
+		if (alumno == null) {
+			throw new NullPointerException("ERROR: El alumno no puede ser nulo.");
+		}
+		List<Cita> citasAlumno = new ArrayList<>();
+		for (Cita cita : coleccionCitas) {
+			if (cita.getAlumno().equals(alumno)) {
+				citasAlumno.add(new Cita(cita));
+			}
+		}
+		Comparator<Profesor> comparadorProfesor = Comparator.comparing(Profesor::getDni);
+		Comparator<Tutoria> comparadorTutoria = Comparator.comparing(Tutoria::getProfesor, comparadorProfesor).thenComparing(Tutoria::getNombre);
+		Comparator<Sesion>comparadorSesion = Comparator.comparing(Sesion::getTutoria,comparadorTutoria).thenComparing(Sesion::getFecha);
+		citasAlumno.sort(Comparator.comparing(Cita::getSesion,comparadorSesion).thenComparing(Cita::getHora));
+		return citasAlumno;
+	}
+
+	@Override
+	public int getTamano() {
+		return coleccionCitas.size();
+	}
+
+	@Override
+	public void insertar(Cita cita) throws OperationNotSupportedException {
+		if (cita == null) {
+			throw new NullPointerException("ERROR: No se puede insertar una cita nula.");
+		}
+		int indice = coleccionCitas.indexOf(cita);
+
+		if (indice == -1) {
+			coleccionCitas.add(new Cita(cita));
+		} else {
+			throw new OperationNotSupportedException("ERROR: Ya existe una cita con esa hora.");
+		}
+	}
+
+	@Override
+	public Cita buscar(Cita cita) {
+		if (cita == null) {
+			throw new IllegalArgumentException("ERROR: No se puede buscar una cita nula.");
+		}
+		int indice = coleccionCitas.indexOf(cita);
+
+		if (indice == -1) {
+			return null;
+		} else {
+			return new Cita(coleccionCitas.get(indice));
+		}
+
+	}
+
+	@Override
+	public void borrar(Cita cita) throws OperationNotSupportedException {
+		if (cita == null) {
+			throw new IllegalArgumentException("ERROR: No se puede borrar una cita nula.");
+		}
+		int indice = coleccionCitas.indexOf(cita);
+
+		if (indice == -1) {
+			throw new OperationNotSupportedException("ERROR: No existe ninguna cita con esa hora.");
+		} else {
+			coleccionCitas.remove(indice);
+		}
+	}
+
+}
