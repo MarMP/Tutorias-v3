2256a716b6ee095097d3ebf66a07c5541327c1a6
Mar <Mar@DESKTOP-7OECT65.home>
2020-02-21 10:52:32 +0100

    Métodos Comenzar y terminar en Alumnos

--- src/main/java/org/iesalandalus/programacion/tutorias/mvc/modelo/negocio/ficheros/Alumnos.java
+++ src/main/java/org/iesalandalus/programacion/tutorias/mvc/modelo/negocio/ficheros/Alumnos.java
@@ -1,5 +1,13 @@
 package org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.ficheros;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -10,7 +18,7 @@ import org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.IAlumnos;
 
 public class Alumnos implements IAlumnos {
 
-	private static final String  NOMBRE_FICHERO_ALUMNOS = "datos/alumnos.dat";
+	private static final String NOMBRE_FICHERO_ALUMNOS = "datos/alumnos.dat";
 	private List<Alumno> coleccionAlumnos;
 
 	public Alumnos() {
@@ -19,8 +27,52 @@ public class Alumnos implements IAlumnos {
 	}
 
 	@Override
+	public void comenzar() {
+		leer();
+	}
+
+	private void leer() {
+		File ficheroAlumnos = new File(NOMBRE_FICHERO_ALUMNOS);
+		try (ObjectInputStream entrada = new ObjectInputStream(new FileInputStream(ficheroAlumnos))) {
+			Alumno alumno = null;
+			do {
+				alumno = (Alumno) entrada.readObject();
+				insertar(alumno);
+			} while (alumno != null);
+		} catch (ClassNotFoundException e) {
+			System.out.println("No puedo encontrar la clase que tengo que leer.");
+		} catch (FileNotFoundException e) {
+			System.out.println("No puedo abrir el fihero de alumnos.");
+		} catch (EOFException e) {
+			System.out.println("Fichero alumnos leído satisfactoriamente.");
+		} catch (IOException e) {
+			System.out.println("Error inesperado de Entrada/Salida.");
+		} catch (OperationNotSupportedException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Override
+	public void terminar() {
+		escribir();
+	}
+
+	private void escribir() {
+		File ficheroAlumnos = new File(NOMBRE_FICHERO_ALUMNOS);
+		try (ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream(ficheroAlumnos))) {
+			for (Alumno alumno : coleccionAlumnos)
+				salida.writeObject(alumno);
+			System.out.println("Fichero alumnos escrito satisfactoriamente.");
+		} catch (FileNotFoundException e) {
+			System.out.println("No puedo crear el fichero de alumnos.");
+		} catch (IOException e) {
+			System.out.println("Error inesperado de Entrada/Salida.");
+		}
+	}
+
+	@Override
 	public List<Alumno> get() {
-		List <Alumno> alumnosOrdenados = copiaProfundaAlumnos();
+		List<Alumno> alumnosOrdenados = copiaProfundaAlumnos();
 		alumnosOrdenados.sort(Comparator.comparing(Alumno::getCorreo));
 		return alumnosOrdenados;
 	}
